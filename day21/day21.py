aac2c350c9026341a67a38df1d66028fac3d9c13
ggbond9952 <2652112575@qq.com>
2026-08-09 23:44:57 +0800

    第二十一天(完善版)
    喜欢喝酒还管不住嘴喜欢乱喷粪的，最后还说为什么别人不理他能不能都死远点

--- day21/day21.py
+++ day21/day21.py
@@ -5,6 +5,7 @@ Author:ignorant-fool
 Version:0.1
 Date:2026/8/8
 """
+import re
 
 # project100 - 验证输入用户名和QQ号是否有效并给出对应的提示信息
 
@@ -34,7 +35,43 @@ Date:2026/8/8
 # 联通号段：130/131/132/155/156/185/186/145/176；
 # 移动号段：134/135/136/137/138/139/150/151/152/157/158/159/182/183/184/187/188/147/178；
 
-import re
+# import re
+#
+# # 创建正则表达式对象，使用了前瞻和回顾来保证手机号前后不应该再出现数字
+# pattern = re.compile(r'(?<=\D)1[34578]\d{9}(?=\D)')
+# sentence = '''重要的事情说8130123456789遍，我的手机号是13512346789这个靓号，
+# 不是15600998765，也不是110或119，王大锤的手机号才是15600998765。'''
+# # 方法一：查找所有匹配并保存在一个列表中
+# tels_list = re.findall(pattern, sentence)
+# for tel in tels_list:
+#     print(tel)
+# print('--------华丽的分隔线--------')
+#
+# # 方法二：通过迭代器取出匹配对象并获得匹配的内容
+# for temp in pattern.finditer(sentence):
+#     print(temp.group())
+# print('--------华丽的分隔线--------')
+#
+# # 方法三：通过search函数指定搜索位置找出所有匹配
+# m = pattern.search(sentence)
+# while m:
+#     print(m.group())
+#     m = pattern.search(sentence, m.end())
 
-# 创建正则表达式对象，使用了前瞻和回顾来保证手机号前后不应该再出现数字
-pattern = re.compile(r'')
+
+# project102 - 替换字符串中的不良内容
+
+# import re
+#
+# sentence = 'Oh, shit! 你是傻逼吗? Fuck you.'
+# purified = re.sub('fuck|shit|[傻煞沙][比笔逼叉缺吊碉雕]', '*', sentence, flags=re.IGNORECASE)
+# print(purified)
+
+
+# project103 - 拆分长字符串
+
+# poem = '窗前明月光，疑是地上霜。举头望明月，低头思故乡。'
+# sentence_list = re.split(r'[，。]', poem)
+# sentence_list = [sentence for sentence in sentence_list if sentence]
+# for sentence in sentence_list:
+#     print(sentence)
