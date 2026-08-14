2e6447da998ed1bb29edbc77c3499c401db330b5
ggbond9952 <2652112575@qq.com>
2026-08-14 23:41:04 +0800

    第二十六天
    拖沓症太严重了，今天还没把第三十一天的内容学完（主要这一节全是知识，大多都是干货），今天水位下去了，我感觉都是我一瓢一瓢的功劳，你要问我为什么一瓢一瓢的取水？我家现在还在停水，取水用来冲马桶o(╥﹏╥)o

---
+++ day26/day26.py
@@ -0,0 +1,93 @@
+"""
+day26
+
+Author:ignorant-fool
+Version:0.1
+Date:2026/8/14
+"""
+
+# project115 - filter, map 以及它们的替代品
+
+# items1 = list(map(lambda x: x ** 2, filter(lambda x: x % 2, range(1, 10))))
+# items2 = [x ** 2 for x in range(1, 10) if x % 2]
+#
+# print(items1)
+# print(items2)
+
+
+# project116 - 装饰器函数
+
+# from functools import wraps
+# import time
+#
+# # #输出函数执行时间的装饰器
+# # def record_time(func):
+# #     """自定义装饰函数的装饰器"""
+# #
+# #     @wraps(func)
+# #     def wrapper(*args, **kwargs):
+# #         start = time.time()
+# #         result = func(*args, **kwargs)
+# #         print(f'{func.__name__}:{time.time() - start}')
+# #         return result
+# #
+# #     return wrapper
+#
+# # 如果装饰器不希望跟print函数耦合，可以编写可以参数化的装饰器。
+#
+# ## 第一种
+# # def record(output):
+# #     """可以参数化的装饰器"""
+# #
+# #     def decorate(func):
+# #
+# #         @wraps(func)
+# #         def wrapper(*args, **kwargs):
+# #             start = time.time()
+# #             result = func(*args, **kwargs)
+# #             print(f'{func.__name__}:{time.time() - start}')
+# #             return result
+# #
+# #         return wrapper
+# #
+# #     return decorate
+#
+# ## 第二种
+# class Record():
+#     """通过定义类的方法定义装饰器"""
+#
+#     def __init__(self, output):
+#         self.output = output
+#
+#     def __call__(self, func):
+#
+#         @wraps(func)
+#         def wrapper(*args, **kwargs):
+#             start = time.time()
+#             result = func(*args, **kwargs)
+#             print(f'{func.__name__}:{time.time() - start}')
+#             return result
+#
+#         return wrapper
+#
+# # @record_time
+# # @record("我是传入的参数")
+# @Record('咕咕嘎嘎')
+# def ceshi():
+#     """测试装饰器"""
+#
+#     i = 13
+#     while True:
+#         time.sleep(1)
+#         print(i)
+#         i += 1
+#         if i == 15:
+#             break
+#
+# def main():
+#     ceshi()
+#     print(ceshi.__name__)
+#     print(ceshi.__doc__)
+#
+# if __name__ == '__main__':
+#     main()
