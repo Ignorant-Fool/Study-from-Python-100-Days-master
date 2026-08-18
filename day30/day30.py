a06905187c46224353fe161ffa708ea6078176ad
ggbond9952 <2652112575@qq.com>
2026-08-18 23:26:57 +0800

    第三十天
    今天中午参加我表弟的升学宴去了，下午在老家干农活，晚上回来都八九点了，在床上眯到十点十四开始起来学习，然后听见我爸妈在外边嘀咕，我感觉我都快疯了，有点PTSD的感觉，每次他俩在外边嘀咕我都感觉我爸又在跟我妈说我都多大了没有自理生活的能力，我真服了，我感觉他的意思是活的跟他一样才叫有自理生活的能力，吃饭不能点外卖，点外卖就是没有自理生活能力，每天要拖地扫地，姿势跟他不一样的就是没有自理生活能力，之前他还质问我是不是他俩老了就是请护工的命，我服了，他还嫌弃上护工不好了，没扔养老院不交钱都是我孝顺，当年我住宿的时候我打电话他都不愿意接，我丢家里个东西先骂一顿然后看心情送不送，今天晚上回来的时候他还搁那说我在车上表现的很困，到家又是打开电脑玩到十二点(合着我学习叫玩电脑，牛批)，关键我又困的不行，直接睡觉我估计他又给我穿小鞋说我故意跟他说的相反，跟他作对。今天早上做梦

---
+++ day30/day30.py
@@ -0,0 +1,34 @@
+"""
+day30
+
+Author:ignorant-fool
+Version:0.1
+Date:2026/8/18
+"""
+
+# project121  - 混入
+
+# 自定义字典限制只有在指定的key不存在时才能在字典中设置键值对
+
+# class SetOnceMappingMixin:
+#     """自定义混入类"""
+#     __slots__ = ()
+#
+#     def __setitem__(self, key, value):
+#         if key in self:
+#             raise KeyError(str(key) + ' already set')
+#         return super().__setitem__(key, value)
+#
+#
+# class SetOnceDict(SetOnceMappingMixin, dict):
+#     """自定义字典"""
+#     pass
+#
+#
+# my_dict = SetOnceDict()
+# try:
+#     my_dict['username'] = 'Luffy'
+#     my_dict['username'] = 'hellokitty'
+# except KeyError:
+#     pass
+# print(my_dict)
